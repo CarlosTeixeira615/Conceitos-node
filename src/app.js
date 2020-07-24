fbec5367a984ff9baa80aaa0a195b4b32e24babb
CarlosTeixeira615 <carlosoteixeira.tc@hotmail.com>
2020-07-23 21:24:26 -0300

    falta a alteraçao do like

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,51 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (req, res) => {
+ const {title } = req.query
+ const results = title 
+ ? repositories.filter(project => project.title.includes(title))
+ :repositories
+ return res.json(results)
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body
+  const repositorie = {id: uuid(), title, url, techs, like:0}
+  repositories.push(repositorie)
+  return res.json(repositorie)
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", (req, res) => {
+  const { id } = req.params
+  const { title, url, techs,like } = req.body
+  const projectIndex = repositories.findIndex(repositories => repositories.id == id)
+if (projectIndex < 0){
+  return res.status(400).json({ error: "Project not found"})
+}
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    like
+  }
+  repositories[projectIndex] = repositorie
+  return res.json(repositorie)
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", (req, res) => {
+  const {id} = req.params
+  const projectIndex = repositories.findIndex(repositories => repositories.id == id)
+  if (projectIndex < 0) {
+    return res.status(400).json({ error: "Project not found" });
+  }
+  repositories.splice(projectIndex, 1)
+  return res.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like",  (req, res) => {
+  
 });
-
+  
 module.exports = app;
